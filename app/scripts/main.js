802937032326930ada4bc0c0c23799f039ba8745
[Thien Nghi] <[nguyenthiennghi1@gmail.com]>
2022-03-16 13:47:53 +0700

    test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,68 @@
-console.log('\'Allo \'Allo!');
+var colName = document.querySelector(".name-col");
+var colCapital = document.querySelector(".capital-col");
+var colFlag = document.querySelector(".flag-col");
+var colCurrency = document.querySelector(".currency-col");
+var colPopulation = document.querySelector(".population-col")
+var colIndex = document.querySelector(".index-col")
 
-// Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+fetch("https://restcountries.com/v3.1/all")
+  .then((response) => response.json())
+  .then((data) => {
+    // sort A -> Z
+    for (let i = 0; i < data.length; i++) {
+      for (let j = i + 1; j < data.length; j++) {
+        if (data[i].name.common > data[j].name.common) {
+          let tmp = data[i];
+          data[i] = data[j];
+          data[j] = tmp;
+        }
+      }
+    }
+    data.forEach((ele , ind) => {
+      console.log(ind);
+      //country
+      let country = document.createElement("div");
+      country.classList.add("element", "ele-Country");
+      country.innerText = ele.name.common;
+      //capital
+      let capital = document.createElement("div");
+      capital.classList.add("element", "ele-Capital");
+      capital.innerText = ele.capital;
+      //flag
+      let flag = document.createElement("div");
+      flag.classList.add("element", "img-cont");
+      let tmp = `            
+      
+        <img src="${ele.flags.svg}" alt="" srcset="">
+   
+      `;
+      flag.innerHTML=tmp;
+      
+      //currency
+      let currency = document.createElement("div");
+      currency.classList.add("element", "ele-Currency");
+     
+      if (typeof ele.currencies !== "undefined"){
+        currency.innerText = Object.keys(ele.currencies)[0]+"-"+Object.values(ele.currencies)[0].name+"-"+Object.values(ele.currencies)[0].symbol
+      }else{
+        currency.innerText = "unknown"
+      }
+      //population
+      let population = document.createElement("div");
+      population.classList.add("element", "ele-Population");
+      population.innerText = (ele.population).toLocaleString();
+      //Index
+      let index = document.createElement("div");
+      index.classList.add("element", "ele-index");
+      index.innerText = ind+1;
 
-// Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+      colName.appendChild(country);
+      colCapital.appendChild(capital);
+      colFlag.appendChild(flag);
+      colCurrency.appendChild(currency)
+      colPopulation.appendChild(population)
+      colIndex.appendChild(index);
+    });
+    // console.log(data[0].name.common)
+    // console.log(data[0].capital[0])
+  });
